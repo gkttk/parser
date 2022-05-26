38869030765dce462bbff9637a3101120c4904c3
Kirill Sviridov <Kirill_Sviridov@epam.com>
2022-05-26 17:59:22 +0300

    Second commit

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,21 @@
 import java.io.FileNotFoundException;
+import java.io.IOException;
 
 import javax.xml.bind.JAXBException;
 
 import model.Dependencies;
 import reader.ReportReader;
+import report.ReportCreator;
 
 public class Main {
 
 
-    public static void main(String[] args) throws JAXBException, FileNotFoundException {
+    public static void main(String[] args) throws JAXBException, IOException {
 
         final Dependencies result = new ReportReader().readReport();
         System.out.println(result);
+
+        new ReportCreator().createReport(result);
 
 
     }
--- src/report/ReportCreator.java
+++ src/report/ReportCreator.java
@@ -1,9 +1,66 @@
 package report;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.List;
+
+import model.Dependencies;
+import model.Dependency;
+import model.Vulnerability;
+
 public class ReportCreator {
 
 
-    public void createReport() {}
+    public void createReport(final Dependencies dependencies) throws IOException {
 
+        FileWriter fWriter = new FileWriter("fileName.html");
+        BufferedWriter writer = new BufferedWriter(fWriter);
+        writer.write("<table>");
+        writer.write("<tr><th>File Name</th><th>Vulnerability</th><th>Severity</th><th>Weakness</th></tr>");
+
+        for (final Dependency dependency : dependencies.getDependencies()) {
+
+            List<Vulnerability> vulnerabilities = dependency.getVulnerabilities();
+
+            //todo
+            if (vulnerabilities != null && !vulnerabilities.isEmpty()) {
+                writer.write("<tr>");
+                for (Vulnerability vulnerability : vulnerabilities) {
+                    final String fileNameCell = generateFileNameCell(dependency);
+                    writer.write(fileNameCell);
+                    String source = vulnerability.getSource();
+                    String name = vulnerability.getName();
+
+                    final String vulnerabilityCell = String.format("<td>%s | %s<td>", source, name);
+                    writer.write(vulnerabilityCell);
+                    writer.write("</tr>");
+                }
+            }
+
+            //  writer.newLine(); //this is not actually needed for html files - can make your code more readable though
+
+            writer.write("</tr>");
+        }
+
+        writer.write("</table>");
+
+        writer.close(); //make sure you close the writer object
+
+    }
+
+
+    private String generateFileNameCell(final Dependency dependency) {
+
+        return String.format("<td>%s</td>", dependency.getFileName());
+    }
+
+    private String generateLine(final Dependency dependency) {
+
+        String fileName = dependency.getFileName();
+        String filePath = dependency.getFilePath();
+
+        return String.format("<li>FileName: %s | FilePath: %s</li>", fileName, filePath);
+    }
 
 }
